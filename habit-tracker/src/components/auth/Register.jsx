b7fd062dc17bf58df81b5d0093196af74fd69fae
Jose Germán Martínez <jgermanmm@gmail.com>
2025-03-07 11:09:35 -0300

    estado de value de unput username

--- habit-tracker/src/components/auth/Register.jsx
+++ habit-tracker/src/components/auth/Register.jsx
@@ -61,7 +61,7 @@ const Register = ({ onRegister }) => {
                 required
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                 placeholder="Username"
-                value={formData.email}
+                value={formData.username}
                 onChange={handleChange}
               />
             </div>
